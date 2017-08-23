b0eba41f48dfd4b421fb0f940d9f9481d427403c
Dylan Stark <dstark04@yahoo.com>
2017-08-23 19:25:19 +0000

    Lab1.1 submission

--- lab11/lab11.cpp
+++ lab11/lab11.cpp
@@ -2,14 +2,15 @@ Step 1. Score starts at 10.
 Step 2. Generate random number.
 Step 3. Generate a second random number.
 Step 4. Select one of the two random numbers randomly.
-Step 5. Analyze input value from user.
-Step 6. If the number the user guesses is the correct number, display "You Win!",
-display score, and go back to step 1.
-Step 7. If the number from the user's input is higher than the number selected
+Step 5. Display "Guess Number."
+Step 6. Analyze input value from user.
+Step 7. If the number the user guesses is the correct number, display "You Win!",
+display score, and go back to step 2.
+Step 8. If the number from the user's input is higher than the number selected
 display "Guess lower".
-Step 8. If the number from the user's input is lower than the number selected
+Step 9. If the number from the user's input is lower than the number selected
 display "Guess higher".
-Step 9. If the user guess incorrectly, subtract 1 from score, Display score, 
-Go back to step 7 until user guesses correctly, in which case you refer to step
-6.
+Step 10. If the user guess incorrectly, subtract 1 from score, Display score, 
+Go back to step 8 until user guesses correctly, in which case you refer to step
+7.
 
