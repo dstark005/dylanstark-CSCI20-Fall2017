1d6763c693edded093910c87bd1f46fbfc9848a7
Dylan Stark <dstark04@yahoo.com>
2017-08-24 00:04:42 +0000

    Lab1.1 submission

---
+++ lab11/activity.cpp
@@ -0,0 +1,17 @@
+//Created By: April Browne
+//Created On: 8/22/2016
+ 
+ #include <iostream>  //   preprocessor directive
+ using namespace std;  //   preprocessor directive
+ 
+ int main()  //   main function
+ {
+     cout<<"Hello World!"<<endl;
+ }
+ 
+ //Identify the following parts of the program:
+ //   preprocessor directives
+ //   main function is to display Hello World
+ //   comments
+ //update it so that it is human readable
+ //add a second output line that says "Hello YourName!" //  comments
